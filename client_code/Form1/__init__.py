4cbc12ae6efa430fe32c3e16abe1e24bd244346d
andreas.daul@uni-tuebingen.de <andreas.daul@uni-tuebingen.de>
2024-01-03 13:23:46 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,26 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import plotly.graph_objects as go
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
+
+  c = FileLoader()
+  if c.file != None:
+    self.ris_data.source = c.file
+    
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    result = anvil.server.call('get_uber_data', file)
+    print(result)
+  
